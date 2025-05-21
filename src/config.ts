55b74b99c7aaa5fd18f97cfa0319ce7bca04e5b4
Simonius <simon.deruyter@outlook.com>
2025-05-21 13:04:20 +0000

    Update of the banner

--- src/config.ts
+++ src/config.ts
@@ -16,12 +16,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/antipolygon-youtube-i9pqOAcQPow-unsplash.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/pexels-eberhardgross-730981.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "Brown House in Between of Mountains", // Credit text to be displayed
+			url: "https://www.pexels.com/photo/brown-house-in-between-of-mountains-730981/", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
